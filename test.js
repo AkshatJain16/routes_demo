dc5908764c518f790a4470fe13a664f6d3fc8350
AkshatJain16 <akshatjain0223@gmail.com>
2026-08-23 19:33:49 +0530

    display on screen

--- test.js
+++ test.js
@@ -1,75 +1,71 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
 
-    if(req.url=='/'){
-        //form
+    if (req.url == '/') {
 
-        res.setHeader('Content-type', 'text/html');
+        // Read the most recent message from the file
 
-        res.end(
-            `
-            <form action="/message" method="POST">
-            <label>Name: </label>
-            <input type="text" name="username"></input>
-            <button type="submit">Add</button>
-            </form>
-            
-            `
-        )
-    }else{
-        if(req.url=='/message'  && req.method==='POST'){
-            res.setHeader('Content-type','text/html');
+        fs.readFile('FormValues.txt', (err, data) => {
 
-            let dataChunks=[];
-            req.on('data',(chunks)=>{
-      
-                dataChunks.push(chunks);
-            })
+            let storedData = '';
 
-            req.on('end',()=>{
-                let buffer = Buffer.concat(dataChunks);
-                console.log(buffer);
-
-                let formData = buffer.toString();
-                console.log(formData);
-
-                const formValues = formData.split('=')[1];
-
-                fs.writeFile('FormValues.txt', formValues, (err)=>{
-                    if(err){
-                        console.log(err);
-                        return;
-                    }
-                    
-                    res.statusCode = 302 //redirected
-                    res.setHeader('Location', '/');
-                    res.end();
-                })
-            })
-        }
-        else{
-            if(req.url==='/read'){
-                //read from the file
-
-                fs.readFile('FormValues.txt',(err,data)=>{
-                    console.log(data.toString());
-
-                    res.end(
-                        `
-                        <h1>${data.toString()}</h1>
-                        `
-                    );
-                })
+            if (!err) {
+                storedData = data.toString();
             }
-        }
-    }
-})
 
-let port =3000;
-server.listen(port, ()=>{
+            res.setHeader('Content-type', 'text/html');
+
+            res.end(
+                `
+                <p>${storedData}</p>
+
+                <form action="/message" method="POST">
+                <label>Name: </label>
+                <input type="text" name="username">
+                <button type="submit">Add</button>
+                </form>
+                `
+            );
+        });
+
+    } else if (req.url == '/message' && req.method == 'POST') {
+
+        let dataChunks = [];
+
+        req.on('data', (chunks) => {
+            dataChunks.push(chunks);
+        });
+
+        req.on('end', () => {
+
+            let buffer = Buffer.concat(dataChunks);
+            let formData = buffer.toString();
+
+            const formValues = formData.split('=')[1];
+
+            // Replace previous message with the new one
+            fs.writeFile('FormValues.txt', formValues, (err) => {
+
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+
+                // Redirect back to the form
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                res.end();
+            });
+        });
+    }
+});
+
+let port = 3000;
+
+server.listen(port, () => {
     console.log('server is running');
-})
+});
