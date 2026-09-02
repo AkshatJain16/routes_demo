26e29530102df0bf0885f7d0ab9c496d5e7ddac0
AkshatJain16 <akshatjain0223@gmail.com>
2026-09-02 19:02:00 +0530

    middleware implementation

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 
+//Custom middleware
+const userMiddleware = (req, res, next) => {
+    req.user = "Guest";
+    next();
+};
+
+//Welcome route
+app.get('/welcome', userMiddleware, (req, res) => {
+    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+});
+
 app.listen(3000, () => {
-    console.log("Server is up and running on port 3000! Ready to handle requests.");
-})
+    console.log('Server is running on port 3000');
+});
