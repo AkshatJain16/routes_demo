54c58fbe7035e29ca3db5d3455b9c182e90cf29c
AkshatJain16 <akshatjain0223@gmail.com>
2026-09-02 23:58:37 +0530

    Dynamic routing

--- app.js
+++ app.js
@@ -2,29 +2,11 @@ const express = require('express');
 
 const app = express();
 
-//GET /products
-app.get('/products', (req, res) => {
-    res.send("Here is the list of all products.")
-});
+app.get('/welcome/:username', (req, res) => {
+    const username = req.params.username;
+    const role = req.query.role;
 
-//POST /products
-app.post('/products', (req, res) => {
-    res.send("A new product has been added.")
-});
-
-//GET  /categories
-app.get('/categories', (req, res) => {
-    res.send("Here is the list of all categories.")
-});
-
-//POST /categories
-app.post('/categories', (req, res) => {
-    res.send("A new category has been created.")
-});
-
-//WildCard Route
-app.use((req, res) => {
-    res.status(404).send("<h1>404 - Page Not Found</h1>");
+    res.send(`Welcome ${username}! Your role is ${role}.`);
 });
 
 app.listen(4000, () => {
