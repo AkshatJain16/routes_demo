54cd7be0915276dbb9ec291403fb07231366dccd
AkshatJain16 <akshatjain0223@gmail.com>
2026-09-05 16:30:23 +0530

    Crud implementation in express

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const express = require('express');
 
 const app = express();
 
-const bookRouter = require('./routes/books');
+const coursesRouter = require('./routes/courses');
+const studentsRouter = require('./routes/students');
 
+//Home Route
+app.get('/', (req, res) => {
+    res.send(`Welcome to the Student and Course Portal API!`);
+});
 
-app.use('/books', bookRouter);
+//Courses Route
+app.use('/courses', coursesRouter);
 
+//Students Route
+app.use('/students', studentsRouter);
+
+//Invalid Route
+app.use((req, res) => {
+    res.status(404).send(`Page not found!`);
+});
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
