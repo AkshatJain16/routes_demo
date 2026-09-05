b764360e40d4c707383de50a951f0bb04dbd2022
AkshatJain16 <akshatjain0223@gmail.com>
2026-09-05 15:34:31 +0530

    router created

--- app.js
+++ app.js
@@ -2,28 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log(`${req.method} request made to ${req.url}`);
-    next();
-});
+const orderRouter = require('./routes/orders');
+const userRouter = require('./routes/users');
 
-app.get('/products', (req, res) => {
+app.use('/orders', orderRouter);
+app.use('/users', userRouter);
 
-    res.send(`Here is the list of all products`);
-});
-
-app.post('/products', (req, res) => {
-    res.send(`A new product has been created`);
-});
-
-app.get('/categories', (req, res) => {
-
-    res.send(`Here is the list of all categories`);
-});
-app.post('/categories', (req, res) => {
-    res.send(`A new category has been created`);
-});
-
-app.listen(4000, () => {
-    console.log('Server is running on http://localhost:4000');
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
